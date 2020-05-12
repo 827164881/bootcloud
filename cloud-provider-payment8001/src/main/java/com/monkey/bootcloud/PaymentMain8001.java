d9bc7c1ae512e9d2f49d68dd67cad5225a5ac47d
luffy <827164881@qq.com>
2020-05-12 23:30:58 +0800

    修改yml配置，添加主启动类

---
+++ cloud-provider-payment8001/src/main/java/com/monkey/bootcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.monkey.bootcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: monkey.G
+ * @create: 2020-05-12 23:28
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentMain8001.class,args);
+  }
+
+}
