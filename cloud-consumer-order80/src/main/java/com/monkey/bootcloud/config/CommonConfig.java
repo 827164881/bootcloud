164acb69154de2c569587eb092fcade3271056b4
luffy <827164881@qq.com>
2020-05-13 11:52:47 +0800

    向Spring容器中注册restTemplate

---
+++ cloud-consumer-order80/src/main/java/com/monkey/bootcloud/config/CommonConfig.java
@@ -0,0 +1,21 @@
+package com.monkey.bootcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description: 通用配置类
+ * @author: monkey.G
+ * @create: 2020-05-13 11:32
+ **/
+@Configuration
+public class CommonConfig {
+
+  @Bean
+  public RestTemplate restTemplate(){
+    return  new RestTemplate();
+  }
+
+}
