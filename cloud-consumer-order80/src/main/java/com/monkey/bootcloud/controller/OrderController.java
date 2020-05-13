4658f510eeddc2ba95144844065e94e7a4970a02
luffy <827164881@qq.com>
2020-05-13 11:56:57 +0800

    通过restTemplate调用服务

---
+++ cloud-consumer-order80/src/main/java/com/monkey/bootcloud/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.monkey.bootcloud.controller;
+
+import com.monkey.bootcloud.common.HttpResult;
+import com.monkey.bootcloud.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description: 订单类，通过restTemplete调用接口
+ * @author: monkey.G
+ * @create: 2020-05-13 11:23
+ **/
+@RestController
+@Slf4j
+public class OrderController {
+  public static final String PAYMENY_URL = "http://localhost:8001";
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @PostMapping("/consumer/payment/create")
+  public HttpResult<Payment> create(Payment payment) {
+    return restTemplate.postForObject(PAYMENY_URL + "payment/create", payment, HttpResult.class);
+  }
+  @GetMapping("/consumer/payment/get/{id}")
+  public HttpResult<Payment> getPayment(@PathVariable("id") Integer id) {
+    log.info("ceshi 热部署是否好使2");
+    return restTemplate.getForObject(PAYMENY_URL + "payment/"+id, HttpResult.class);
+  }
+
+}
