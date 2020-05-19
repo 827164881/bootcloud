40b28aaab7a4f25943bc7a6815d9e49a65c19c07
luffy <827164881@qq.com>
2020-05-19 21:59:11 +0800

    consul消费的pom、yml、main

---
+++ cloud-consumer-order-consul80/src/main/java/com/monkey/bootcloud/CloudConsumerOrderConsul80Application.java
@@ -0,0 +1,18 @@
+package com.monkey.bootcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 消费的主启动类
+ * @author: monkey.G
+ * @create: 2020-05-13 11:20
+ **/
+@SpringBootApplication
+public class CloudConsumerOrderConsul80Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudConsumerOrderConsul80Application.class,args);
+  }
+
+}
