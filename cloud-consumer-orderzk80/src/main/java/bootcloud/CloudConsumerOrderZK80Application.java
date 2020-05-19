6d29716f9727e177c0b916b96b46fa545cf45c2f
luffy <827164881@qq.com>
2020-05-19 10:40:33 +0800

    创建zookper的消费端，并成功调用程序

---
+++ cloud-consumer-orderzk80/src/main/java/bootcloud/CloudConsumerOrderZK80Application.java
@@ -0,0 +1,18 @@
+package bootcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 消费的主启动类
+ * @author: monkey.G
+ * @create: 2020-05-13 11:20
+ **/
+@SpringBootApplication
+public class CloudConsumerOrderZK80Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CloudConsumerOrderZK80Application.class,args);
+  }
+
+}
---
+++ cloud-consumer-orderzk80/src/main/java/bootcloud/config/CommonConfig.java
@@ -0,0 +1,22 @@
+package bootcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description: 通用配置类
+ * @author: monkey.G
+ * @create: 2020-05-13 11:32
+ **/
+@Configuration
+public class CommonConfig {
+
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate(){
+    return  new RestTemplate();
+  }
+
+}
