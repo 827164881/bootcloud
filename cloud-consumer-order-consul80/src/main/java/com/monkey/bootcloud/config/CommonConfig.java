abb1ffc50b529caba7cec2525636721753689a63
luffy <827164881@qq.com>
2020-05-19 21:59:43 +0800

    consul消费端的业务逻辑

---
+++ cloud-consumer-order-consul80/src/main/java/com/monkey/bootcloud/config/CommonConfig.java
@@ -0,0 +1,23 @@
+package com.monkey.bootcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description: 通用配置类
+ * @author: monkey.G
+ * @create: 2020-05-13 11:32
+ **/
+@Configuration
+public class CommonConfig {
+
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate(){
+    return  new RestTemplate();
+  }
+
+}
---
+++ cloud-consumer-order-consul80/src/main/java/com/monkey/bootcloud/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.monkey.bootcloud.controller;
+
+import com.monkey.bootcloud.common.HttpResult;
+import com.monkey.bootcloud.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description: 订单类，通过restTemplete调用接口
+ * @author: monkey.G
+ * @create: 2020-05-13 11:23
+ **/
+@RestController
+@Slf4j
+public class OrderController {
+//  public static final String PAYMENY_URL = "http://localhost:8001";
+  public static final String REMOTE_SERVICE_NAME = "http://cloud-consumer-payment/";
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @RequestMapping("/consumer/consul")
+  public String create(Payment payment) {
+    return restTemplate.getForObject(REMOTE_SERVICE_NAME + "payment/consul", String.class);
+  }
+
+
+}
