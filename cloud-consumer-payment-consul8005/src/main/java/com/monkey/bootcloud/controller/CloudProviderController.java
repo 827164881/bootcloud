1523be4e3b52b1db5335748fda7396f68ec3d0fb
luffy <827164881@qq.com>
2020-05-19 21:58:05 +0800

    consul的生产端业务逻辑

---
+++ cloud-consumer-payment-consul8005/src/main/java/com/monkey/bootcloud/controller/CloudProviderController.java
@@ -0,0 +1,25 @@
+package com.monkey.bootcloud.controller;
+
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: controller
+ * @author: monkey.G
+ * @create: 2020-05-18 11:59
+ **/
+@RestController
+@Slf4j
+public class CloudProviderController {
+  @Value("${server.port}")
+  private String serverPort;
+
+  @RequestMapping(value = "/payment/consul")
+  public String paymentzk(){
+    return "springcloud with consul: " + serverPort + "\t" + UUID.randomUUID().toString();
+  }
+
+}
