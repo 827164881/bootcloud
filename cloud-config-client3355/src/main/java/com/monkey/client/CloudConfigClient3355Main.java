8807df61a89b0fad251298baf0e11140612b900c
MonkeyG <827164881@qq.com>
2020-06-03 23:44:13 +0800

    客户端从配置中心拉取配置

---
+++ cloud-config-client3355/src/main/java/com/monkey/client/CloudConfigClient3355Main.java
@@ -0,0 +1,12 @@
+package com.monkey.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigClient3355Main {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClient3355Main.class,args);
+    }
+}
